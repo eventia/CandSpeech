99bd670ef9dd1f4c2934787867e49dda215878e1
eventia <eventia@gmail.com>
2017-05-08 14:37:26 +0900

    1st Candidate Speech WoodCloud

--- wordcloud.py
+++ wordcloud.py
@@ -22,10 +22,11 @@ r = lambda: random.randint(0,255)
 color = lambda: (r(), r(), r())
 
 def get_bill_text(billnum):
-    url = 'http://pokr.kr/bill/%s/text' % billnum
+#    url = 'http://pokr.kr/bill/%s/text' % billnum
+    url = 'https://raw.githubusercontent.com/eventia/CandSpeech/master/%s.txt' % billnum
     response = urlopen(url).read().decode('utf-8')
     page = html.fromstring(response)
-    text = page.xpath(".//div[@id='bill-sections']/pre/text()")[0]
+    text = response
     return text
 
 def get_tags(text, ntags=50, multiplier=10):
@@ -35,17 +36,14 @@ def get_tags(text, ntags=50, multiplier=10):
     return [{ 'color': color(), 'tag': n, 'size': c*multiplier }\
                 for n, c in count.most_common(ntags)]
 
-def draw_cloud(tags, filename, fontname='NanumGothic', size=(800, 600)):
+def draw_cloud(tags, filename, fontname='NanumGothic', size=(1280, 720)):
     pytagcloud.create_tag_image(tags, filename, fontname=fontname, size=size)
     webbrowser.open(filename)
 
+bill_num = ['n1_moon','n2_hong','n3_ahn','n4_yoo','n5_sim', ]
 
-#bill_num = '1904882'
-#bill_num ='1918652'
-
-bill_num = '1918651'
-
-text = get_bill_text(bill_num)
-tags = get_tags(text)
-print(tags)
-draw_cloud(tags, 'wordcloud_'+bill_num+'.png')
+for candperson in bill_num:
+    text = get_bill_text(candperson)
+    tags = get_tags(text)
+    print(tags)
+    draw_cloud(tags, 'speech_'+candperson+'.png')
